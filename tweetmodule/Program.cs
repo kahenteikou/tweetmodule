6f82b07c56b2f4c44ef4fea5ecdee80f7f5119c6
kokkiemouse <kokkiemouse@fascode.net>
2021-05-30 20:24:08 +0900

    added file

---
+++ tweetmodule/Program.cs
@@ -0,0 +1,31 @@
+﻿using CoreTweet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweetmodule
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Tokens token;
+            if(args.Length < 5)
+            {
+                return 1;
+            }
+            try
+            {
+                token = Tokens.Create(args[0], args[1], args[2], args[3]);
+                token.Statuses.Update(args[4]);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
